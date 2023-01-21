82819c394594e5d9350d719b54faa50f81d99be0
Dimannn <diman1992nn@gmail.com>
2023-01-21 18:09:31 +0300

    add FormDate

--- src/main/java/ru/netology/FormDate.java
+++ src/main/java/ru/netology/FormDate.java
@@ -1,7 +1,9 @@
 package ru.netology;
 
+import java.util.Date;
+
 public class FormDate {
-    public String day;
-    public String month;
-    public String year;
+    public int day;
+    public int month;
+    public int year;
 }
